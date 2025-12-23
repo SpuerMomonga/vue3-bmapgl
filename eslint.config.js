a1ce4901e8c86020fa5507a16ce132d738b98d3b
SpuerMomonga <1638829841@qq.com>
2025-12-23 17:44:45 +0800

    chore: initialize pnpm workspace and add TypeScript configuration

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    formatters: true,
+    vue: true,
+    typescript: true,
+  },
+  {
+    // Without `files`, they are general rules for all files
+    rules: {
+      'n/prefer-global/process': 'off',
+      'no-control-regex': 'off',
+      'toml/padding-line-between-pairs': 'off',
+      'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      '@stylistic/brace-style': 'off',
+      'no-redeclare': 'off',
+      '@typescript-eslint/no-redeclare': 'off',
+      'ts/no-unsafe-function-type': 'off',
+      'ts/no-namespace': 'off',
+      'unicorn/error-message': 'off',
+      'pnpm/yaml-enforce-settings': 'off',
+    },
+    ignores: [
+      // ...globs
+    ],
+  },
+)
